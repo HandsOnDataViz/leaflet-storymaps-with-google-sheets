b2b8011aa4fb5d0e96861252c501dd65c1ec2c5d
Erica Y. Hayes <ericay.hayes@gmail.com>
2020-02-26 17:53:29 -0500

    Updated file format of images and changed out url in googleDocURL

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,2 +1,2 @@
 //var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/';
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1IhjP_9Ze112ZrFGUb5qrTvSDRRiaET3ySkekekmqZJw/';
