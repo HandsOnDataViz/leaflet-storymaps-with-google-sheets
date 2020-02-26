7012323eb97ec2d28b1a174c417015fc2ff9392a
Erica Y. Hayes <ericay.hayes@gmail.com>
2020-02-26 15:39:37 -0500

    Updated with Gordon's photos and changed out google-doc-url

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,2 @@
+//var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/';
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/';
