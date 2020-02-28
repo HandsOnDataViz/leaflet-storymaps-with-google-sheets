4b7afddb2c2e5a7e0b21241af178779f96ba1e21
Erica Y. Hayes <ericay.hayes@gmail.com>
2020-02-28 14:50:44 -0500

    Update leaflet-providers.js

--- scripts/leaflet-providers.js
+++ scripts/leaflet-providers.js
@@ -216,7 +216,7 @@
 			}
 		},
 		MapBox: {
-			url: '//api.tiles.mapbox.com/styles/v1/{eyhayesvillanova}/{z}/{x}/{y}.png?access_token={pk.eyJ1IjoiZXloYXllc3ZpbGxhbm92YSIsImEiOiJjanprZmY2dGMwMXEwM29xcGoxdnU3ZGU2In0.fGzcDtt3pRUN0uMp7w-vAA}',
+			url: 'https://api.mapbox.com/styles/v1/eyhayesvillanova/ck0o747481bpy1cnjzuwiqryc/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiZXloYXllc3ZpbGxhbm92YSIsImEiOiJjanprZmY2dGMwMXEwM29xcGoxdnU3ZGU2In0.fGzcDtt3pRUN0uMp7w-vAA',
 				options: {
 				attribution:
 					'Imagery from <a href="http://mapbox.com/about/maps/">MapBox</a> &mdash; ' +
