301624fdc1dca87474cd9520eb998b97ea69c25f
ageor7 <github@dipot.com>
2020-05-15 10:09:23 +0300

    Update google-doc-url.js
    Change original google sheet template from 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/' to 'https://docs.google.com/spreadsheets/d/1o7N0eJ7xoPNXQuo9yT-ZRt0fU9GEIMtIDpH8MmzhOzg/'

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1o7N0eJ7xoPNXQuo9yT-ZRt0fU9GEIMtIDpH8MmzhOzg/';
