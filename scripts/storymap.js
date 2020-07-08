53eaddca042350ae64577c4b4494bff36dcd27de
Erica Y. Hayes <ericay.hayes@gmail.com>
2020-07-08 15:17:04 -0400

    Update storymap.js

--- scripts/storymap.js
+++ scripts/storymap.js
@@ -482,19 +482,21 @@ $(window).on('load', function() {
     if (name && url) {
       if (url.indexOf('@') > 0) { url = 'mailto:' + url; }
       credit += ' <a href="' + url + '">' + name + '</a> | ';
-      credit += ' <a href="' + url + '">' + web + '</a> | ';
     } else if (name) {
       credit += ' by ' + name + ' | ';
-    } else if (web) {
-      credit +=  web + ' | ';
-    }
-      else {
+    } else {
       credit += ' | ';
     }
 
     if (getSetting('projectWebsite')) credit += ' Project website: ' + getSetting('projectWebsite') + ' | ';
     if (getSetting('_githubRepo')) credit += ' GitHub Repo: ' + getSetting('_githubRepo') + ' | ';
     if (getSetting('_webDeveloper')) credit += 'Web Development & Digital Project Support by: ' + getSetting('_webDeveloper') + ' | ';
+
+    
+
+
+
+
     if (getSetting('_codeCredit')) credit += 'Original Code by ' + getSetting('_codeCredit') + ' | ';
     credit += ' | ';
     $('.leaflet-control-attribution')[0].innerHTML = credit + attributionHTML;
