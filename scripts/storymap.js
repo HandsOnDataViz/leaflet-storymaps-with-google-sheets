9749fa76bfe9e4d135ef73b5255dc6bb40b0c935
Erica Y. Hayes <ericay.hayes@gmail.com>
2020-07-08 15:28:04 -0400

    Update storymap.js

--- scripts/storymap.js
+++ scripts/storymap.js
@@ -491,7 +491,7 @@ $(window).on('load', function() {
 
     if (getSetting('projectWebsite')) credit += ' Project website: ' + getSetting('projectWebsite') + ' | ';
     if (getSetting('_githubRepo')) credit += ' GitHub Repo: ' + getSetting('_githubRepo') + ' | ';
-    if (getSetting('_webDeveloper')) credit += 'Digital Project Support by: ' + getSetting('_webDeveloper')' | ';
+    if (getSetting('_webDeveloper')) credit += 'Digital Project Support by: ' + getSetting('_webDeveloper') + ' | ';
     if ( web && weburl) {
       if (url.indexOf('@') > 0) { url = 'mailto:' + url; }
       credit += ' <a href="' + url + '">' + web + '</a>';
