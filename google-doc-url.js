21fa269561f4e9f056725f8976348c686c62a113
ilyankou <ilyankou@gmail.com>
2020-07-31 14:37:52 +0100

    Change tabletop to PapaParse with Google key

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,2 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/edit#gid=0';
+var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
