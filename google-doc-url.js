d9588a06304515c7d8d42b6c38cf9f02888ca50a
ilyankou <ilyankou@gmail.com>
2020-09-10 19:05:48 +0100

    Add logo image & comments to google-doc-url

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,2 +1,5 @@
+// paste in your published Google Sheets URL from the browser address bar
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/edit#gid=0';
-var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
+
+// insert your own Google Sheets API key from https://console.developers.google.com
+var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
