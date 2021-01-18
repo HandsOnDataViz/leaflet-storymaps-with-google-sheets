d19a7d0dbac35a0a94a82b0178293b20c54ead73
ilyankou <ilyankou@gmail.com>
2021-01-18 15:05:46 +0000

    Make hash id correspond to row number

--- scripts/storymap.js
+++ scripts/storymap.js
@@ -290,7 +290,7 @@ $(window).on('load', function() {
         ) {
 
           // Update URL hash
-          location.hash = i;
+          location.hash = i + 2;
 
           // Remove styling for the old in-focus chapter and
           // add it to the new active chapter
@@ -438,7 +438,7 @@ $(window).on('load', function() {
 
     // On first load, check hash and if it contains an number, scroll down
     if (parseInt(location.hash.substr(1))) {
-      var containerId = parseInt( location.hash.substr(1) );
+      var containerId = parseInt( location.hash.substr(1) ) - 2;
       $('#contents').animate({
         scrollTop: $('#container' + containerId).offset().top
       }, 2000);
